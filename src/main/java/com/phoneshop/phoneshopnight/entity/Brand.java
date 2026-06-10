07ed645ce42b5fd24d9725fde8be9a41317c1d1b
Rathana <oeurnratana79@gmail.com>
2026-06-10 12:38:29 +0700

    updat db success

--- src/main/java/com/phoneshop/phoneshopnight/entity/Brand.java
+++ src/main/java/com/phoneshop/phoneshopnight/entity/Brand.java
@@ -1,11 +1,18 @@
 package com.phoneshop.phoneshopnight.entity;
 
-import jakarta.persistence.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Data;
+
+
 
 @Data//auto get ser
 @Entity
+
 public class Brand {
+	    @Id
 		private Integer id;
 		String name;
 		
